4c6220d9a8e053be98f661bf00c2cb7ef717065b
lyj <351675195@qq.com>
2024-04-08 21:16:27 +0800

    fix: 保存到数据库空白表现象

--- apps/add_marks.py
+++ apps/add_marks.py
@@ -1,7 +1,7 @@
 import traceback
 
 from flask import Blueprint, jsonify, request
-from sqlalchemy import text
+from sqlalchemy import text, Table, Column, Integer, String, inspect
 
 from extensions import db
 from models.crossings import Crossings
@@ -71,63 +71,74 @@ def save_as_datasets():
     crossings = request.get_json()['crossings']
 
     try:
-        # 创建数据表
-        db.session.execute(text(
-            'CREATE TABLE `road-mark`.`c_' + table_name + '_sensors`  (\
-            `id` int NOT NULL AUTO_INCREMENT,\
-            `lng` varchar(60) NULL DEFAULT NULL,\
-            `lat` varchar(60) NULL DEFAULT NULL,\
-            `group` int NULL DEFAULT NULL,\
-            `group_number` int NULL DEFAULT NULL,\
-            PRIMARY KEY (`id`))'))
+        if not inspect(db.engine).has_table("c_" + table_name + "_sensors"):
+            # 新建传感器表
+            dynamic_table1 = Table(
+                "c_" + table_name + "_sensors",
+                db.MetaData(),
+                Column('id', Integer, primary_key=True),
+                Column('lng', String(60)),
+                Column('lat', String(60)),
+                Column('group', Integer),
+                Column('group_number', Integer)
+            )
+            dynamic_table1.create(bind=db.engine, checkfirst=True)
 
-        db.session.execute(text(
-            'CREATE TABLE `road-mark`.`c_' + table_name + '_gateways`  (\
-            `id` int NOT NULL AUTO_INCREMENT,\
-            `lng` varchar(60) NULL DEFAULT NULL,\
-            `lat` varchar(60) NULL DEFAULT NULL,\
-            `group` int NULL DEFAULT NULL,\
-            `group_number` int NULL DEFAULT NULL,\
-            PRIMARY KEY (`id`))'))
+            # 新建网关表
+            dynamic_table2 = Table(
+                "c_" + table_name + "_gateways",
+                db.MetaData(),
+                Column('id', Integer, primary_key=True),
+                Column('lng', String(60)),
+                Column('lat', String(60)),
+                Column('group', Integer),
+                Column('group_number', Integer)
+            )
+            dynamic_table2.create(bind=db.engine, checkfirst=True)
 
-        db.session.execute(text(
-            'CREATE TABLE `road-mark`.`c_' + table_name + '_crossings`  (\
-            `id` int NOT NULL AUTO_INCREMENT,\
-            `lng` varchar(60) NULL DEFAULT NULL,\
-            `lat` varchar(60) NULL DEFAULT NULL,\
-            PRIMARY KEY (`id`))'))
+            # 新建路口表
+            dynamic_table3 = Table(
+                "c_" + table_name + "_crossings",
+                db.MetaData(),
+                Column('id', Integer, primary_key=True),
+                Column('lng', String(60)),
+                Column('lat', String(60)),
+            )
+            dynamic_table3.create(bind=db.engine, checkfirst=True)
+        else:
+            return jsonify({
+                'success': False,
+                'code': HTTP_ERROR_OPERATION,
+                'msg': '数据库中有同名数据表，请修改名称！',
+                'data': None,
+            })
 
-    except Exception:
-        traceback.print_exc()
-        return jsonify({
-            'success': False,
-            'code': HTTP_ERROR_OPERATION,
-            'msg': '数据库中有同名数据表，请修改名称！',
-            'data': None,
-        })
-
-
-    try:
         # 传感器存入
         for index, group in enumerate(sensors):
             for i, s in enumerate(group):
-                db.session.add(Sensors(lng=s['lng'], lat=s['lat'], group=index + 1, group_number=i + 1))
+                db.session.execute(dynamic_table1.insert().values(
+                    lng=s['lng'], lat=s['lat'], group=index + 1, group_number=i + 1
+                ))
         # 网关存入
         for index, group in enumerate(gateways):
             for i, s in enumerate(group):
-                db.session.add(Gateways(lng=s['lng'], lat=s['lat'], group=index + 1, group_number=i + 1))
+                db.session.execute(dynamic_table2.insert().values(
+                    lng=s['lng'], lat=s['lat'], group=index + 1, group_number=i + 1
+                ))
         # 路口存入
         for c in crossings:
-            db.session.add(Crossings(lng=c['lng'], lat=c['lat']))
+            db.session.execute(dynamic_table3.insert().values(lng=c['lng'], lat=c['lat']))
 
         # 提交事务
         db.session.commit()
+
         return jsonify({
             'success': True,
             'code': HTTP_SUCCESS,
             'msg': '保存成功！',
             'data': None,
         })
+
     except Exception:
         traceback.print_exc()
         return jsonify({
