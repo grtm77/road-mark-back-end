3ef1503f230e6b77fc90f4b2ba936151599d4b3e
lyj <351675195@qq.com>
2024-04-10 02:27:52 +0800

    feat: 增加跨域设定

--- extensions.py
+++ extensions.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 import config
 
 app = Flask(__name__)
 app.config.from_object(config)
+CORS(app)
 db = SQLAlchemy(app)
 
